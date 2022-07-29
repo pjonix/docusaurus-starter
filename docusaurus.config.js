8fc1923512555764d0a5729f64a1dda63d08f204
pjonix <46640889+pjonix@users.noreply.github.com>
2022-07-29 21:55:46 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Hio海鸥',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
