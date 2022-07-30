d368368d5eb98d152f0ad8f5d0a1a6654b27bbb8
pjonix <46640889+pjonix@users.noreply.github.com>
2022-07-30 13:26:14 +0800

    mathjax try

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
 };
+
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,21 +121,3 @@ const config = {
 };
 
 module.exports = config;
-
-window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']]
-  },
-  svg: {
-    fontCache: 'global'
-  }
-};
-
-(function () {
-  var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
-  script.async = true;
-  document.head.appendChild(script);
-})();
-
-
