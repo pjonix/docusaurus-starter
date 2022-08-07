5991673a29d4fc5593dfbde17b87ed9d2dda8c02
pjonix <46640889+pjonix@users.noreply.github.com>
2022-08-07 17:33:49 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/Reflection/intro">
             请进->
           </Link>
         </div>
